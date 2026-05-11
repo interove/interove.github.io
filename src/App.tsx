bb487bb7cd631dc361da6908074bc0a407c8ac54
interove <camefom@qq.com>
2026-05-11 20:35:25 +0800

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,10 @@
 import Home from "./pages/Home";
 
 export default function App() {
-  return <Home />;
+  try {
+    return <Home />;
+  } catch (error) {
+    console.error("App crashed:", error);
+    return <div>出错了：{String(error)}</div>;
+  }
 }
